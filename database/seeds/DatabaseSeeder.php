47787a091903620dfe337cd40def6b2fe206ce2a
renchao <admin@w7.com>
2021-12-16 16:43:06 +0800

    补充缺失的文件

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use W7\DatabaseTool\Seed\Seeder;
+
+class DatabaseSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		//
+		$this->call([
+
+		]);
+	}
+}
